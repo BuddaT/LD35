9f3518b7f62f820c3a7b28637b5c183f80446e09
Taufiq Hoven <taufiq.hoven@gmail.com>
2016-04-16 10:59:21 +1000

    Initial generated project

---
+++ desktop/src/net/buddat/ludumdare/ld35/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.buddat.ludumdare.ld35.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.buddat.ludumdare.ld35.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
